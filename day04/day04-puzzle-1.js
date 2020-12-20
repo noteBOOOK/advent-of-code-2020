6df43d15e3a9523454345ad8a5f88fdfaf7413bd
noteBOOOK <jason.long77@hotmail.com>
2020-12-20 18:30:12 -0500

    Finished day 4 puzzle 1

--- day04/day04-puzzle-1.js
+++ day04/day04-puzzle-1.js
@@ -5,14 +5,30 @@ const file = readFile('./day04-data', 'utf-8').split('\n\n');
 const data = file.map((e) => e.split(/[ '\n']+/));
 
 const checkPassport = data => {
-
+  validPassports = 0;
+  
   data.forEach(passport => {
-    console.log("NEW PASSPORT!!!!!!!!!!!!!!!!!!: ");
+    const passportValidations = {
+      byr: false,
+      iyr: false,
+      eyr: false,
+      hgt: false,
+      hcl: false,
+      ecl: false,
+      pid: false,
+    }
     passport.forEach(validations => {
-      console.log(validations)
+      [validation, value] = validations.split(":")
+      if (passportValidations.hasOwnProperty(validation)) {
+        passportValidations[validation] = true;
+      }
     })
-  })
 
+    if (Object.values(passportValidations).every((value) => value === true)) {
+      validPassports++;
+    }
+  })
+  return validPassports;
 }
 
-checkPassport(data);
+console.log(checkPassport(data));
