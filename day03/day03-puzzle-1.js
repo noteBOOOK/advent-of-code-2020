3c097df5a0e2bd3551cbafa949149c2727957295
noteBOOOK <jason.long77@hotmail.com>
2020-12-20 16:58:12 -0500

    started foundation of function, need to fix tree count

---
+++ day03/day03-puzzle-1.js
@@ -0,0 +1,30 @@
+const readFile = require('fs').readFileSync;
+
+const file = readFile('./day03-data', 'utf-8').split('\n');
+
+const countTrees = data => {
+
+  let treeCount = 0;
+  let x = 0;
+  let y = 0;
+
+  const xMax = data[0].split('').length - 1;
+  const yMax = data.length - 1;
+
+  while (y < yMax) {
+    x += 3;
+    y++;
+
+    if (x > xMax) {
+      x -= xMax;
+    }
+
+    if (data[y][x] === '#'){
+      treeCount++;
+    }
+  }
+  console.log(treeCount);
+
+}
+
+countTrees(file);
