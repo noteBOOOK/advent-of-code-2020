1d8a94726db78450cdfaaa88e9ec2578a75fb275
noteBOOOK <jason.long77@hotmail.com>
2020-12-22 15:11:51 -0500

    Working on height validation

--- day04/day04-puzzle-2.js
+++ day04/day04-puzzle-2.js
@@ -5,48 +5,43 @@ const data = file.map((e) => e.split(/[ '\n']+/));
 
 
 
-const validateBirthYear = year => {
-  if (year >= 1920 && year <= 2002) {
-    return true;
-  }
-  return false;
+const validateYear = (year, min, max) => {
+  return year >= min && year <= max;
 }
 
-const validateIssueYear = year => {
-  if (year >= 2010 && year <= 2020) {
-    return true;
-  }
-  return false;
+const validateHeight = height => {
+  const unit = height.slice(-2);
+  const value = height.slice(0, height.length - 2)
+  console.log(value, unit);
 }
 
+validateHeight('190cm');
 
-const checkPassport = data => {
-  validPassports = 0;
-  data.forEach(passport => {
-    const passportValidations = {
-      byr: false,
-      iyr: false,
-      eyr: false,
-      hgt: false,
-      hcl: false,
-      ecl: false,
-      pid: false,
-    }
+// const checkPassport = data => {
+//   validPassports = 0;
+//   data.forEach(passport => {
+//     const passportValidations = {
+//       byr: false,
+//       iyr: false,
+//       eyr: false,
+//       hgt: false,
+//       hcl: false,
+//       ecl: false,
+//       pid: false,
+//     }
 
-    passport.forEach(validations => {
-      [validation, value] = validations.split(":")
-      // if (passportValidations.hasOwnProperty(validation)) {
-      //   passportValidations[validation] = true;
-      // }
-      console.log("CHECK HERE!!!!!!!", validation, value);
-    })
+//     passport.forEach(validations => {
+//       [validation, value] = validations.split(":")
+//       // if (passportValidations.hasOwnProperty(validation)) {
+//       //   passportValidations[validation] = true;
+//       // }
+//       console.log("CHECK HERE!!!!!!!", validation, value);
+//     })
 
-    if (Object.values(passportValidations).every((value) => value === true)) {
-      validPassports++;
-    }
-  })
+//     if (Object.values(passportValidations).every((value) => value === true)) {
+//       validPassports++;
+//     }
+//   })
   
-  return validPassports;
-}
-
-console.log(checkPassport(data));
+//   return validPassports;
+// }
